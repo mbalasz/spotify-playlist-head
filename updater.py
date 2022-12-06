8dc66ae6b9d1a2b0e08f16576d41c1252c10634c
Mateusz <matti93@gmail.com>
2022-12-06 12:08:40 +0000

    creating a playlist

--- updater.py
+++ updater.py
@@ -8,9 +8,10 @@ from spotipy import SpotifyException
 LIKED_SONGS_TYPE = 'liked_songs'
 PLAYLIST_TYPE = 'playlist'
 
-scope = "user-library-read"
+scope = "user-library-read,playlist-modify-public,playlist-modify-private"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+current_user_id = sp.current_user()['id']
 
 class Track:
     def __init__(self, id, name, added_at):
@@ -19,49 +20,56 @@ class Track:
         self.added_at = added_at
 
 class Playlist:
-    def __init__(self, name, tracks):
+    def __init__(self, id, name, tracks):
+        self.id = id
         self.name = name
         self.tracks = tracks
 
-class PlaylistFetcher:
-    def fetch(self, playlist_to_mirror_config) -> Playlist:
-        pass
-
-    def fetch_with_name(self, name) -> [Playlist]:
-        pass
-
-class RegularPlaylistFetcher(PlaylistFetcher):
-    def fetch(self, playlist_to_mirror_config):
+class PlaylistRepository:
+    def get_playlist_from_config(self, playlist_to_mirror_config):
         playlist = sp.playlist(playlist_to_mirror_config['id'])
         return Playlist(
+            playlist['id'],
             playlist['name'],
             self._get_tracks_from_playlist(playlist))
-    
-    def fetch_with_name(self, name):
+            
+    def get_playlists_with_name(self, name):
         playlists = sp.current_user_playlists()['items']
         return [
             Playlist(
+                playlist['id'],
                 playlist['name'],
                 self._get_tracks_from_playlist(sp.playlist(playlist['id']))
             ) 
             for playlist 
             in filter(lambda p: p['name'] == name, playlists)
         ]
+    
+    def get_or_create_playlist_with_name(self, name):
+        playlists = self.get_playlists_with_name(name)
+        if len(playlists) > 0:
+            print("Playlist with name {} exists".format(name))
+            return playlists
+        print("Playlist with name {} doesn't exist. Creating new one".format(name))
+        created_playlist_json = sp.user_playlist_create(current_user_id, name)
+        return [Playlist(
+            created_playlist_json['id'],
+            name,
+            []
+        )]
 
+    def get_liked_songs():
+        saved_tracks = sp.current_user_saved_tracks()
+        return Playlist(
+            saved_tracks['id'],
+            'Liked Songs',
+            [item['track']['id'] for item in saved_tracks['items']])
 
     def _get_tracks_from_playlist(self, playlist):
         return [
             Track(item['track']['id'], item['track']['name'], item['added_at']) 
             for item 
             in playlist['tracks']['items']]
-
-
-class LikedSongsFetcher(PlaylistFetcher):
-    def fetch(self, playlist_to_mirror_config):
-        saved_tracks = sp.current_user_saved_tracks()
-        return Playlist(
-            'Liked Songs',
-            [item['track']['id'] for item in saved_tracks['items']])
 
 class CopyPlaylistFactory:
     def create_playlist(self, original_playlist, playlist_config) -> Playlist:
@@ -71,10 +79,11 @@ class CopyTracksFromDatePlaylistFactory(CopyPlaylistFactory):
     def create_playlist(self, original_playlist, playlist_config):
         from_date = playlist_config['from_date']
         tracks = self.get_tracks_from_date(original_playlist, from_date)
-        return Playlist("{} [from {}]".format(original_playlist.name, from_date), tracks)
+        return Playlist(None, "{} [from {}]".format(original_playlist.name, from_date), tracks)
 
     def get_tracks_from_date(self, original_playlist, date):
-        pass
+        sorted_tracks = sorted(original_playlist.tracks, key=lambda t: t.added_at)
+        return [track for track in sorted_tracks if track.added_at >= date]
 
 class CopyNumRecentTracksPlaylistFactory(CopyPlaylistFactory):
     def create_playlist(self, original_playlist, playlist_config):
@@ -83,10 +92,10 @@ class CopyNumRecentTracksPlaylistFactory(CopyPlaylistFactory):
             raise Exception(
                 "Can't create Playlist from this configuration. Missing `count` key: {}".format(playlist_config))
         recent_tracks = self.get_recent_tracks(original_playlist, count)
-        return Playlist("{} [last {}]".format(original_playlist.name), recent_tracks)
+        return Playlist(None, "{} [last {}]".format(original_playlist.name, count), recent_tracks)
     
     def get_recent_tracks(self, original_playlist, count):
-        return sorted(original_playlist.tracks, key=lambda t: t.date_added)[-count:]
+        return sorted(original_playlist.tracks, key=lambda t: t.added_at)[-count:]
 
 
 
@@ -94,16 +103,18 @@ for config in [
     {
         "type": "playlist",
         # "id": "3OI8krl8FxMZsyzJSa8LUM",
-        "id": "6YWdIfKcfus3hSjtpZKngh",
-        "from_date": "2019-01-01"
+        # "id": "6YWdIfKcfus3hSjtpZKngh",
+        "id": "0nGiC07XVUxBqOGd407pBO",
+        # "from_date": "2022-06-23",
+        "most_recent_count": 112
     }]:
+    playlist_repository = PlaylistRepository()
     if config['type'] == LIKED_SONGS_TYPE:
-        playlist_fetcher = LikedSongsFetcher()
+        original_playlist = playlist_repository.get_liked_songs()
     elif config['type'] == PLAYLIST_TYPE:
-        playlist_fetcher = RegularPlaylistFetcher()
+        original_playlist = playlist_repository.get_playlist_from_config(config)
     else:
         raise Exception("Unrecognized type")
-    original_playlist = playlist_fetcher.fetch(config)
 
     if 'from_date' in config:
         factory = CopyTracksFromDatePlaylistFactory()
@@ -111,16 +122,26 @@ for config in [
         factory = CopyNumRecentTracksPlaylistFactory()
     else:
         raise Exception("No strategy for copy in config: {}".format(config))
-    copy_playlist = factory.create_playlist(original_playlist, config)
-
-    existing_playlists = RegularPlaylistFetcher().fetch_with_name(copy_playlist.name)
-    if len(existing_playlists) == 1:
-        print("Playlist with name {} exists, deleting existing tracks".format(copy_playlist.name))
-        existing_playlist = existing_playlists[0]
-        for track in existing_playlist.tracks:
-            print("Removing track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
-    elif len(existing_playlists) == 0:
-        print("Playlist with name {} doesn't exist, will create new one".format(copy_playlist.name))
-    else:
-        raise Exception("More than one playlist with the name of the copy playlist")
+    mirror_playlist = factory.create_playlist(original_playlist, config)
+    playlists = playlist_repository.get_or_create_playlist_with_name(mirror_playlist.name)
+    if len(playlists) > 1:
+        raise Exception("More than one playlist with the name of the mirror playlist")
+    if len(playlists) == 0:
+        raise Exception("Playlist not created")
+    playlist_to_populate = playlists[0]
+    sp.user_playlist_remove_all_occurrences_of_tracks(
+        current_user_id,
+        playlist_to_populate.id, 
+        [t.id for t in playlist_to_populate.tracks])
+    print(len(mirror_playlist.tracks))
+    for t in range(0, len(mirror_playlist.tracks), 100):
+        print("HEY: {}".format([t.id for t in mirror_playlist.tracks[t:100]]))
+    sp.user_playlist_add_tracks(current_user_id, playlist_to_populate.id, [t.id for t in mirror_playlist.tracks])
+    # if len(existing_playlists) == 1:
+    #     print("Playlist with name {} exists, deleting existing tracks".format(copy_playlist.name))
+    #     existing_playlist = existing_playlists[0]
+    #     for track in existing_playlist.tracks:
+    #         print("Removing track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
+    #     sp.user_playlist_remove_all_occurrences_of_tracks(sp.current_user(), existing_playlist.id, existing_playlist.tracks) 
+    
     # use copy_playlist_id to add tracks to it.
