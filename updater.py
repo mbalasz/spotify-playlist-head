b66901a13c5c5fd409718fed0142c5c97dc62ba4
Mateusz <matti93@gmail.com>
2023-01-22 18:11:31 +0000

    Fix positions of tracks in the playlist

--- updater.py
+++ updater.py
@@ -42,7 +42,7 @@ class PlaylistRepository:
         return Playlist(
             playlist['id'],
             playlist['name'],
-            self._get_tracks_from_playlist(playlist['tracks']))
+            self._get_tracks_from_playlist(playlist['name'], playlist['tracks']))
             
     def get_playlists_with_name(self, name):
         existing_playlists = sp.current_user_playlists()['items']
@@ -50,7 +50,7 @@ class PlaylistRepository:
             Playlist(
                 playlist['id'],
                 playlist['name'],
-                self._get_tracks_from_playlist(sp.playlist(playlist['id'])['tracks'])
+                self._get_tracks_from_playlist(playlist['name'], sp.playlist(playlist['id'])['tracks'])
             ) 
             for playlist 
             in filter(lambda p: p['name'] == name, existing_playlists)
@@ -74,26 +74,26 @@ class PlaylistRepository:
         return Playlist(
             None,
             'Liked Songs',
-            self._get_tracks_from_playlist(saved_tracks)
+            self._get_tracks_from_playlist("Liked songs", saved_tracks)
         )
 
-    def _get_tracks_from_playlist(self, playlist_tracks):
+    def _get_tracks_from_playlist(self, playlist_name, playlist_tracks):
         result = spotipy_items_to_tracks(playlist_tracks['items'])
         while playlist_tracks['next']:
-            print("fetching tracks...: {}".format(len(result)))
+            print("Fetched {} tracks from {}".format(len(result), playlist_name))
             playlist_tracks = sp.next(playlist_tracks)
             result.extend(spotipy_items_to_tracks(playlist_tracks['items']))
         return result
 
 class CopyPlaylistFactory:
-    def create_playlist(self, original_playlist, playlist_config) -> Playlist:
+    def create_playlist(self, original_playlist) -> Playlist:
         pass
 
 class TracksFromDateCopyPlaylistFactory(CopyPlaylistFactory):
     def __init__(self, from_date):
         self.from_date = from_date
 
-    def create_playlist(self, original_playlist, playlist_config):
+    def create_playlist(self, original_playlist):
         tracks = self.get_tracks_from_date(original_playlist, self.from_date)
         return Playlist(None, "{} [from {}]".format(original_playlist.name, self.from_date), tracks)
 
@@ -105,12 +105,14 @@ class NumRecentTracksCopyPlaylistFactory(CopyPlaylistFactory):
     def __init__(self, num_recent_tracks):
         self.num_recent_tracks = num_recent_tracks
 
-    def create_playlist(self, original_playlist, playlist_config):
+    def create_playlist(self, original_playlist):
         recent_tracks = self._get_recent_tracks(original_playlist, self.num_recent_tracks)
         return Playlist(None, "{} [last {}]".format(original_playlist.name, self.num_recent_tracks), recent_tracks)
     
     def _get_recent_tracks(self, original_playlist, count):
-        return sorted(original_playlist.tracks, key=lambda t: t.added_at)[-count:]
+        tracks = sorted(original_playlist.tracks, key=lambda t: t.added_at)[-count:]
+        tracks.reverse()
+        return tracks
 
 for config in [
     {
@@ -134,39 +136,51 @@ for config in [
         factory = NumRecentTracksCopyPlaylistFactory(config['num_recent_tracks'])
     else:
         raise Exception("No strategy for copy in config: {}".format(config))
-    mirror_playlist = factory.create_playlist(original_playlist, config)
+    mirror_playlist = factory.create_playlist(original_playlist)
 
     existing_playlists = playlist_repository.get_or_create_playlist_with_name(mirror_playlist.name)
     if len(existing_playlists) > 1:
-        raise Exception("More than one playlist with the name of the mirror playlist")
+        raise Exception("More than one playlist with the name of the mirror playlist already exists")
     if len(existing_playlists) == 0:
-        raise Exception("Playlist not created")
+        raise Exception("Mirror playlist not created")
     playlist_to_populate = existing_playlists[0]
     tracks_to_remove = [
         track 
         for track 
         in playlist_to_populate.tracks 
-        if track.id not in [copy_track.id for copy_track in mirror_playlist.tracks]]
+        if track.id not in [mirror_track.id for mirror_track in mirror_playlist.tracks]]
     tracks_to_add = [
-        track
-        for track
-        in mirror_playlist.tracks
+        (idx, track)
+        for idx, track
+        in enumerate(mirror_playlist.tracks)
         if track.id not in [existing_track.id for existing_track in playlist_to_populate.tracks]
     ]
-    tracks_to_add.reverse()
 
-    print("Tracks to remove: {}".format(len(tracks_to_remove)))
-    print("Tracks to add: {}".format(len(tracks_to_add)))
-    # for track in tracks_to_remove:
-    #     print("Removing track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
-    # for track in tracks_to_add:
-    #     print("Adding track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
+    print("Removing {} tracks.".format(len(tracks_to_remove)))
+    print("Adding {} tracks.".format(len(tracks_to_add)))
+    for track in tracks_to_remove:
+        print("Removing track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
+    for idx, track in tracks_to_add:
+        print("Adding track: ({}, {}), added at:{}".format(track.name, track.id, track.added_at))
     sp.user_playlist_remove_all_occurrences_of_tracks(
         current_user_id,
         playlist_to_populate.id, 
         [track.id for track in tracks_to_remove])
-    for i in range(0, len(tracks_to_add), 100):
+
+    if len(tracks_to_add) > 0:
+        # Adding tracks in batches with consecutive indices to preserve the same ordering of tracks.
+        first_track_in_batch_idx = 0
+        for i in range(1, len(tracks_to_add)):
+            if tracks_to_add[i][0] - tracks_to_add[i-1][0] > 1 or i - first_track_in_batch_idx >= 100:
+                sp.user_playlist_add_tracks(
+                    current_user_id,
+                    playlist_to_populate.id,
+                    [track.id for idx, track in tracks_to_add[first_track_in_batch_idx:i]],
+                    position = tracks_to_add[first_track_in_batch_idx][0])
+                first_track_in_batch_idx = i
+
         sp.user_playlist_add_tracks(
             current_user_id,
             playlist_to_populate.id,
-            [track.id for track in tracks_to_add[i:i+100]])
+            [track.id for idx, track in tracks_to_add[first_track_in_batch_idx:len(tracks_to_add)]],
+            position = tracks_to_add[first_track_in_batch_idx][0])
